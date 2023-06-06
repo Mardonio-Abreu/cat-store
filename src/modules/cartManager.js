6578fd076fb9db248ef70978432334a4d8795b09
Mardonio-Abreu <mardonio.abreu@protonmail.com>
2023-06-06 12:00:54 -0600

    CartManager draft

--- src/modules/cartManager.js
+++ src/modules/cartManager.js
@@ -1,1 +1,32 @@
 //Class that manages the shopping carts
+
+class cartManager {
+    constructor(){
+
+        const product = {
+            id: '0',
+            quantity: 0
+        }
+
+        this.products = [];
+    }
+
+    addProduct(id){
+
+        this.products.map((product) => {
+            if(product.id === id){
+                product.quantity++
+            }else{
+                product.id = id;
+                product.quantity = 1;
+                this.products.push(product);
+                return;
+            }
+        })
+
+       
+
+    }
+}
+
+module.exports = cartManager;
